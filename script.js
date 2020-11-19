98a8f824d8d67c27d3dc07a2ed13fdc1883ff093
Boris Sumarokov <bor1999@mail.ru>
2020-11-19 12:13:31 +0300

    Урок 01

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+// ---- Переменные (не константы) -----------
+let money;
+let income;
+let addExpenses;
+let deposit;
+let mission;
+let period;
 
+// ---- Выводим alert -----------------------
+alert('GLO JavaScript Learning!');
+
+// ---- Выводим в консоль -------------------
+console.log('Hello World !!!!');
