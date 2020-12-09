b4de0e15c69583fd41426df0bdff3a556abf0786
Boris Sumarokov <bor1999@mail.ru>
2020-12-09 22:29:03 +0300

    Урок 15 обязательный часть 2

---
+++ script.js
@@ -0,0 +1,28 @@
+'use strict';
+
+class First {
+   constructor() {
+
+   }
+
+   hello() {
+      console.log('Привет! Я метод родителя!');
+   }
+}
+
+class Second extends First {
+   constructor() {
+      super();
+   }
+
+   hello() {
+      super.hello();
+      console.log('А я наследуемый метод!');
+   }
+}
+
+
+const second = new Second();
+second.hello();
+
+
